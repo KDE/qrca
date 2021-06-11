084da35fede6d7aaada80c5fc237df6aa5971233
Jonah Brüchert <jbb@kaidan.im>
2021-06-11 14:03:12 +0200

    Convert to SPDX

--- src/Qrca.h
+++ src/Qrca.h
@@ -1,16 +1,9 @@
 /*
- *  Qrca is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ *  SPDX-FileCopyrightText: 2019 Jonah Brüchert <jbb@kaidan.im>
+ *  SPDX-FileCopyrightText: 2019 Simon Schmeisser <s.schmeisser@gmx.net>
+ *  SPDX-FileCopyrightText: 2020 Nicolas Fella <nicolas.fella@gmx.de>
  *
- *  Qrca is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this project.  If not, see <http://www.gnu.org/licenses/>.
+ *  SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 #ifndef QRCA_H
